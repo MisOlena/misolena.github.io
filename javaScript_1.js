ac7b223c7456a3ce686ca19c5b8498bb9ddf93a5
MisOlena <38257277+MisOlena@users.noreply.github.com>
2018-04-16 22:27:06 +0300

    Add files via upload

---
+++ javaScript_1.js
@@ -0,0 +1,12 @@
+function desk(){
+	var masAnsver = ["1956", "2", "57834", "1135", "405230", "56373"];
+	var n=document.forma1.namber.value;
+	var r=document.forma1.ansver.value;
+	
+	if (masAnsver[n-1] == r) alert("Вірно");
+	else alert("Пробуй ще раз");
+
+	
+}	
+	
+
